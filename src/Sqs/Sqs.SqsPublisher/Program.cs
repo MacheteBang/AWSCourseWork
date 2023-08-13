1000c1d6d638f0ffe2614c8639fa31ac416279dd
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2023-08-13 08:40:03 -0400

    1:🚚Move to New Folder

---
+++ src/Sqs/Sqs.SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Sqs.SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "jonathan@mmmpizza.net",
+    FullName = "Jonathan Gregorsky",
+    DateOfBirth = new DateTime(1977, 10, 07),
+    GitHubUsername = "MacheteBang"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
