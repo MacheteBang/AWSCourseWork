80ccc910f6f8d9be7882d3f61b155e64c04c810d
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2023-08-18 16:43:09 -0400

    4:⭐Intro to S3 Playgound

---
+++ src/S3Playground/Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.S3.Model;
+using Amazon.S3;
+
+var s3Client = new AmazonS3Client();
+
+await using var inputStream = new FileStream("./Resume.JonathanGregorsky.pdf", FileMode.Open, FileAccess.Read);
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = "jxg-bucket",
+    Key = "docs/Resume.JonathanGregorsky.pdf",
+    ContentType = "application/pdf",
+    InputStream = inputStream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
