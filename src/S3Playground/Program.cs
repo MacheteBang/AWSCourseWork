48681b644ba0d0f038dde66bad33525ea57bfa10
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2023-08-18 16:51:22 -0400

    4:Read file in S3 Playground

--- src/S3Playground/Program.cs
+++ src/S3Playground/Program.cs
@@ -1,16 +1,31 @@
 ﻿using Amazon.S3.Model;
 using Amazon.S3;
+using System.Text;
 
 var s3Client = new AmazonS3Client();
 
-await using var inputStream = new FileStream("./Resume.JonathanGregorsky.pdf", FileMode.Open, FileAccess.Read);
+// // PUT
+// await using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
 
-var putObjectRequest = new PutObjectRequest
+// var putObjectRequest = new PutObjectRequest
+// {
+//     BucketName = "jxg-bucket",
+//     Key = "files/movies.csv",
+//     ContentType = "text/csv",
+//     InputStream = inputStream
+// };
+
+// await s3Client.PutObjectAsync(putObjectRequest);
+
+var getObjectRequest = new GetObjectRequest
 {
     BucketName = "jxg-bucket",
-    Key = "docs/Resume.JonathanGregorsky.pdf",
-    ContentType = "application/pdf",
-    InputStream = inputStream
+    Key = "files/movies.csv"
 };
 
-await s3Client.PutObjectAsync(putObjectRequest);
+var response = await s3Client.GetObjectAsync(getObjectRequest);
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var text = Encoding.Default.GetString(memoryStream.ToArray());
+Console.WriteLine(text);
